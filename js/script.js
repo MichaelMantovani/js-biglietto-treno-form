b5d226d04f44b63b4885f0a0aac0611b8dfeb56f
MichaelMantovani <michmantovani@gmail.com>
2023-05-29 16:10:52 +0200

    Replace prompt with input

--- js/script.js
+++ js/script.js
@@ -5,16 +5,9 @@ console.log('JS OK')
 /* OPERAZIONI PRELIMINARI */
 
 
-//  Recupero elemento dal DOM
-const userWelcome = document.getElementById('user-name');
-const userAgeRange = document.getElementById('user-age');
-const userTravelPrice = document.getElementById('travel-price');
-const userDiscountTravelPrice = document.getElementById('discount-travel-price');
+/* Stabilisco tariffe di prezzo e sconti per età */
 
-
-// Stabilisco tariffe di prezzo e sconti per età
-
-// Percentuale di sconto per maggiorenni
+// Percentuale di sconto per minorenni
 const discountUnderage = 20;
 
 // Percentuale di sconto per over65
@@ -28,50 +21,24 @@ const seniorAge= 65;
 const kmPrice = 0.21; 
 
 
-// Chiedo all'utent il suo nome e salvo la risposta
-const userName = prompt('Come ti chiami?' , 'Fabio');
-userWelcome.innerText = userName;
+
+
+//  Recupero elementi dal DOM
+
+// Chiedo all'utente il suo nome e lo salvo
+const userName = document.getElementById('user-name').value.trim();
 console.log(userName);
 
+// Chiedo all'utente il suo cognome e lo salvo
+const userSurname = document.getElementById('user-surname').value.trim();
+console.log(userSurname);
 
-// Chiedo all'utente il numero di km che vuole percorrere e salvo la risposta
-
-const userTravelDistance = parseInt(prompt('Quanti km vuoi percorrere?' , ' 20')); 
+// Chiedo all'utente quanti km vuole percorrere e li salvo 
+const userTravelDistance = parseInt(document.getElementById('user-travel-distance').value);
 console.log(userTravelDistance);
 
-
-// Chiedo all'utente la sua età e salvo la risposta
-
-const userAge = parseInt(prompt('Qunati anni hai?' , '17'));
+// Chiedo all'utente la sua età e la salvo
+const userAge = parseInt(document.getElementById('user-age').value);
 console.log(userAge);
-
-// Calcolo il costo del viaggio
-
-const travelPrice = userTravelDistance * 0.21;
-userTravelPrice.innerText = travelPrice
-console.log(travelPrice);
-
-
-// Applico gli sconti in base all'età
-
-let discountTravelPrice; 
-let ageRange = 'minorenne'
-
-if (userAge < 18){
-  discountTravelPrice = travelPrice - ((travelPrice * discountUnderage) / 100 )
-} else if (userAge >= seniorAge) {
-  ageRange = 'un over 65'
-  discountTravelPrice = travelPrice - ((travelPrice * discountSenior) / 100)
-} else {
-  ageRange = 'maggiorenne. Putroppo devi esere un over65 oppure un minorenne per accedere agli sconti'
-}
-
-userAgeRange.innerText = ageRange;
-
-
-// Stampo in console il risultato
-console.log (discountTravelPrice.toFixed(2));
-userDiscountTravelPrice.innerText = (discountTravelPrice.toFixed(2));
-
 
 
