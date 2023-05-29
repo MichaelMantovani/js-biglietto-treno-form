4069dc95bcf7100c553c5400a66e7d021a746ef5
MichaelMantovani <michmantovani@gmail.com>
2023-05-29 19:05:09 +0200

    Fix Js indentation

--- js/script.js
+++ js/script.js
@@ -74,50 +74,50 @@ submitButton.addEventListener('click' , function(){
     alert('Alcuni dati inseriti non sono validi')
   } else {
 
-    // Calcolo il prezzo
-  const travelPrice = userTravelDistance * kmPrice;
-  console.log(travelPrice);
+      // Calcolo il prezzo
+    const travelPrice = userTravelDistance * kmPrice;
+    console.log(travelPrice);
 
+    
+    // Applico gli sconti in base alla fascia d'età
+    let discountPrice = travelPrice;
+    let ticketType = 'Biglietto Standard';
+    if (userAge < adultAge) {
+      discountPrice = travelPrice - (travelPrice * discountUnderage); 
+      ticketType = 'Biglietto Ridotto'
+    } else if(userAge >= seniorAge){
+      discountPrice = travelPrice - (travelPrice * discountSenior)
+      ticketType = 'Biglietto over65'
+    }
+    
+    console.log(discountPrice);
+
+    // Genero casualemente un numero per la carrozza
+    const wagonMin = 1;
+    const wagonMax = 20;
+    const userWagon = Math.floor(Math.random() * (wagonMax - wagonMin + 1) + wagonMin);
+    
+
+    // Genero casualemente un codice per il biglietto
+    const cpMin = 10000;
+    const cpMax = 99999;
+    const cpCode = Math.floor(Math.random() * (cpMax - cpMin + 1) + cpMin);
+
+    // Faccio apparire il biglietto solo quando è generato
+    ticket.classList.remove('d-none');
+    
+    // Stampo in pagina il nome e il cognome
+    userNamePlaceholder.innerText = userName + ' ' + userSurname
+
+    // Stampo in pagina il tipo di sconto e il prezzo del biglietto 
+    ticketTypePlaceholder.innerText = ticketType;
+    ticketPricePlaceholder.innerText = (discountPrice.toFixed(2)) + '€'; 
+    
+    // Stampo in pagina i numeri generati 
+    userWagonPlaceholder.innerText = userWagon;
+    cpCodePlaceholder.innerText = cpCode;
   
-  // Applico gli sconti in base alla fascia d'età
-  let discountPrice = travelPrice;
-  let ticketType = 'Biglietto Standard';
-  if (userAge < adultAge) {
-    discountPrice = travelPrice - (travelPrice * discountUnderage); 
-    ticketType = 'Biglietto Ridotto'
-  } else if(userAge >= seniorAge){
-    discountPrice = travelPrice - (travelPrice * discountSenior)
-    ticketType = 'Biglietto over65'
   }
-  
-  console.log(discountPrice);
-
-  // Genero casualemente un numero per la carrozza
-  const wagonMin = 1;
-  const wagonMax = 20;
-  const userWagon = Math.floor(Math.random() * (wagonMax - wagonMin + 1) + wagonMin);
-  
-
-  // Genero casualemente un codice per il biglietto
-  const cpMin = 10000;
-  const cpMax = 99999;
-  const cpCode = Math.floor(Math.random() * (cpMax - cpMin + 1) + cpMin);
-
-  // Faccio apparire il biglietto solo quando è generato
-  ticket.classList.remove('d-none');
-  
-   // Stampo in pagina il nome e il cognome
-   userNamePlaceholder.innerText = userName + ' ' + userSurname
-
-  // Stampo in pagina il tipo di sconto e il prezzo del biglietto 
-  ticketTypePlaceholder.innerText = ticketType;
-  ticketPricePlaceholder.innerText = (discountPrice.toFixed(2)) + '€'; 
-  
-  // Stampo in pagina i numeri generati 
-  userWagonPlaceholder.innerText = userWagon;
-  cpCodePlaceholder.innerText = cpCode;
-  
-}
 
 
 })
